57c741755eee20e98da7a007603b8c7d732b619d
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-06-05 09:56:01 +0200

    1.0.12

--- Package.swift
+++ Package.swift
@@ -2,18 +2,26 @@
 import PackageDescription
 
 let package = Package(
-   name: "Shared",
-   platforms: [
-     .iOS(.v14),
-     .macOS(.v11)
-   ],
-   products: [
-      .library(name: "Shared", targets: ["Shared"])
-   ],
-   targets: [
-      .binaryTarget(
-         name: "Shared",
-         path: "shared.xcframework"
-         )
-   ]
+    name: "Shared",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
+    products: [
+        .library(name: "Shared", targets: ["Shared"]),
+        .library(name: "Shared-Dep", targets: ["Dependencies"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/rickclephas/KMP-NativeCoroutines.git", exact: "1.0.0-ALPHA-31")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Shared",
+            path: "shared.xcframework"
+        ),
+        .target(name: "Dependencies", dependencies: [
+            .product(name: "KMPNativeCoroutinesCore", package: "KMP-NativeCoroutines"),
+            .product(name: "KMPNativeCoroutinesAsync", package: "KMP-NativeCoroutines"),
+        ])
+    ]
 )

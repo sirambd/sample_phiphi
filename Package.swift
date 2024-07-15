b0cfe3ad468244c190dc87c3f9de8e929a39d4d7
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-07-15 15:25:20 +0200

    test release 1.0.23

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/sirambd/sample_phiphi/releases/download/1.0.22/shared.xcframework.zip"
+            url: "https://github.com/sirambd/sample_phiphi/releases/download/1.0.23/shared.xcframework.zip",
+            checksum: "48508c575029d4a8c4d9c13bb2c93e3226ec5d5c680e213e95f37b6d8bd79ef6"
         )
     ]
 )
